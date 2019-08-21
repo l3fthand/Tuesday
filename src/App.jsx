ce732a71b1a34601004fa45210589e7b1aedd879
Anson Wong <270021714@cit3-501m-14.local>
2019-08-21 13:32:48 +1200

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,7 @@ import './App.css';
  class App extends Component{
   constructor(props){
     super(props);
-    this.state={
-      activeView:'projects',
-    }
+    
   }
   setActiveView = (view)=>{
     this.setState({activeView:view})
